3f713e8ef89e5ab5987308000f0c801f18d3476b
jax-parker <parkyjax711@gmail.com>
2023-03-11 10:43:22 +0000

    add game structure

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Import necessary libraries
+#Welcome message
+#User inputs name (input valid?)
+#Define the main function (def conundrum game)
+#Define the variables (word list)
+#Pick random word
+#Scramble word
+#Print scrambled word
+#User input guess (input valid?)
+#Check if guess is correct
+#Print message to user
+#Ask if user would like to play again (input valid?)
+
+'''Python Libraries'''
+import os
+import random
+from random import shuffle
+import time
+
+#Create a list of words to randomly be chosen
+WORDS = ('apple', 'telvision', 'science', 'computer', 'kitchen', 'xylophone')
+
+# 
+# 
